39ef0ed82ecb652aed6b798ab561c46f914d1bb6
Joseph Dayo <joseph.dayo@gmail.com>
2013-12-15 12:04:35 +0800

    Add capability to find within a view heirarchy

--- droiuby/wrappers/view_wrapper.rb
+++ droiuby/wrappers/view_wrapper.rb
@@ -2,19 +2,19 @@ require 'droiuby/wrappers/animation'
 require 'droiuby/wrappers/java_helpers/view_helper'
 
 class ViewWrapper
-  
+
   include JavaMethodHelper
   include Droiuby::ViewHelper
   include Droiuby::Wrappers::Listeners
 
   java_fast_accessor Java::android.view.View, [Java::float], :x, :y
-  
-  java_attr_accessor :right, :left, :top, :bottom, :alpha, :rotation, :pivot_x, :pivot_y, :translation_x, 
-    :translation_y, :scroll_x, :scroll_y, :scale_x, :scale_y, :rotation_x, :rotation_y, :camera_distance,
-    :padding_left, :padding_top, :padding_right, :padding_bottom
-    
+
+  java_attr_accessor :right, :left, :top, :bottom, :alpha, :rotation, :pivot_x, :pivot_y, :translation_x,
+  :translation_y, :scroll_x, :scroll_y, :scale_x, :scale_y, :rotation_x, :rotation_y, :camera_distance,
+  :padding_left, :padding_top, :padding_right, :padding_bottom
+
   java_attr_reader :id, :width, :height, :measured_with, :measured_height, :background
-  
+
   def initialize(view = nil)
     unless view.nil?
       @view = view
@@ -28,11 +28,11 @@ class ViewWrapper
   def native
     @view
   end
-  
+
   def builder
     @builder
   end
-  
+
   def background=(background)
     if background.kind_of? DrawableWrapper
       @view.setBackgroundDrawable(background.native)
@@ -40,15 +40,15 @@ class ViewWrapper
       builder.setProperty(native,'background', background.to_s)
     end
   end
-  
+
   def width=(width)
     builder.setProperty(native,'width', width.to_s)
   end
-  
+
   def height=(height)
     builder.setProperty(native,'height', height.to_s)
   end
-  
+
   def background_color=(value)
     @view.setBackgroundColor(parse_color(value));
   end
@@ -64,23 +64,23 @@ class ViewWrapper
   def invalidate
     @view.invalidate
   end
-  
+
   def hidden?
     @view.getVisibility == Java::android.view.View::GONE
   end
-  
+
   def show!
     @view.setVisibility(Java::android.view.View::VISIBLE)
   end
-  
+
   def hide!
     @view.setVisibility(Java::android.view.View::GONE)
   end
-  
+
   def to_front!
     @view.bringToFront
   end
-  
+
   def visible=(flag)
     @view.setVisibility(flag ? Java::android.view.View::VISIBLE : Java::android.view.View::INVISIBLE)
   end
@@ -100,7 +100,7 @@ class ViewWrapper
   def parent
     wrap_native_view(@view.getParent)
   end
-  
+
   def blink
     orig_alpha = self.alpha
     view = self
@@ -110,7 +110,18 @@ class ViewWrapper
       view.alpha = orig_alpha
     }.start
   end
-  
+
+  def find(selectors)
+    view = _activity_builder.findViewByName(selectors, self.native, false)
+    if (view.kind_of? Java::java.util.ArrayList)
+      view.toArray.to_a.collect do |element|
+        wrap_native_view(element)
+      end
+    else
+      wrap_native_view(view) if view
+    end
+  end
+
   def animate(&block)
     animator = Animator.new(self)
     block.call(animator)
@@ -129,15 +140,15 @@ class ViewWrapper
   def click
     self.native.performClick
   end
-  
+
   def p_tree(level = 0)
     spaces = ''
     level.times { |i| spaces << '  '}
-      
+
     id_attr = self.id
     name_attr = ""
-    class_attr = "" 
-    
+    class_attr = ""
+
     if tag
       id_attr = tag.getView_id
       name_attr = tag.getView_name
@@ -146,17 +157,16 @@ class ViewWrapper
       rid = reverse_resolve(id_attr)
       id_attr = "^#{rid}" unless rid.nil?
     end
-    
+
     data_attribute_list = []
     if _extras
       attributes = _extras.getDataAttributes
-      
+
       attributes.keySet.each do |key|
         data_attribute_list << "data-#{key}=\"#{self.data(key)}\""
       end
     end
-    
-    
+
     puts "#{spaces}#{self.class.name} id=\"#{id_attr}\" name=\"#{name_attr}\" class=\"#{class_attr}\" #{data_attribute_list.join(' ')}\n"
     self.children.each { |c|
       c.p_tree(level + 1)
@@ -172,7 +182,7 @@ class ViewWrapper
     end
     nil
   end
-  
+
   def data(key)
     if _extras
       data_attributes = _extras.getDataAttributes
@@ -183,21 +193,21 @@ class ViewWrapper
   end
 
   protected
-  
+
   def parse_color(color_value)
     if color_value.kind_of? String
       Java::android.graphics.Color.parseColor(color_value);
     else
-      color_value 
+      color_value
     end
   end
-  
+
   def _extras
     unless native.getTag.nil?
       tag = native.getTag
       if tag.kind_of? Java::com.droiuby.client.core.ViewExtras
         tag
       end
-     end
+    end
   end
 end
