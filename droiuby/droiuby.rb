24163d9f8b2a97158f8c0b664b8af191e11f3664
Joseph Dayo <jdayo@friendster.com>
2014-02-06 19:40:05 +0800

    framework update WIP

--- droiuby/droiuby.rb
+++ droiuby/droiuby.rb
@@ -1,30 +1,37 @@
 require 'droiuby/loader'
 
-#Droiuby Framework class  
+#Droiuby Framework class
 class DroiubyFramework
   
   def before_activity_setup
     fname = "#{File.dirname(__FILE__)}/bootstrap.rb"
-    @bootstrap = @bootstrap || File.read(fname) 
+    @bootstrap = @bootstrap || File.read(fname)
     eval(@bootstrap, TOPLEVEL_BINDING, fname, __LINE__)
   end
-  
+
   def preload
     fname = "#{File.dirname(__FILE__)}/preload.rb"
-    @preload = @preload || File.read(fname) 
+    @preload = @preload || File.read(fname)
     eval(@preload, TOPLEVEL_BINDING, fname, __LINE__)
   end
-  
+
   def on_click(view)
   end
-  
+
   def script(controller)
     klass = controller.camelize.constantize
     instance = klass.new
+
+    skip_content_view = if instance.respond_to? :before_content_render
+      instance.before_content_render
+    end
+
+    set_content_view unless skip_content_view
+
     instance.on_create
     instance
   end
-  
+
   def resolve_view(name, node)
     klass = name.constantize
     instance = klass.new
@@ -34,7 +41,7 @@ class DroiubyFramework
       instance
     end
   end
-  
+
 end
 
 $framework = DroiubyFramework.new
---
+++ droiuby/wrappers/listeners/on_web_console_ready_listener.rb
@@ -0,0 +1,21 @@
+module Droiuby
+  module Wrappers
+    module Listeners
+      class OnWebConsoleReadyListener
+        
+        def initialize(execution_bundle, auto_wrap_block)
+          @auto_wrap_block = auto_wrap_block
+          @native = Java::com.droiuby.wrappers::OnWebConsoleReadyRubyWrapper.new(execution_bundle, self)
+        end
+        
+        def onReady(httpd)
+          @auto_wrap_block.call(httpd)
+        end
+        
+        def to_native
+          @native
+        end
+      end
+    end
+  end
+end
